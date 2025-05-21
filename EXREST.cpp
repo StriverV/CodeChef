a94e333eb6e9267e8451091b52bebbd861a71037
Mystic <187439614+StriverV@users.noreply.github.com>
2025-05-21 20:41:17 +0600

    Create EXREST.cpp

---
+++ EXREST.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define Naba ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define ll long long 
+#define bl bool 
+#define nl '\n'
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+using namespace std;
+
+int main()
+{
+    Naba;
+
+    int n; cin >> n;
+	
+	cout << n*3 << endl;
+
+    return 0;
+}
